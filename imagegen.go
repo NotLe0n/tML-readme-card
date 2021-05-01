1483a8be9dd9345130677d670e531a4e19bd1474
NotLe0n <>
2021-05-01 20:10:04 +0000

    added color tag parser

--- imagegen.go
+++ imagegen.go
@@ -10,6 +10,8 @@ import (
 	"path/filepath"
 	"strconv"
 	"time"
+  "regexp"
+  "encoding/hex"
 
 	"github.com/fogleman/gg"
 )
@@ -89,21 +91,20 @@ func run(steamId string, img io.Writer) error {
 
   // get Author name
 	err := getSteamJson("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key="+mySecret+"&steamids="+steamId, &steamjson)
-	if err != nil {
-    
+	if err != nil {  
 		return errors.New("something went wrong")
 	}
 
   // Draw Text
   userNameWidth, _ := dc.MeasureString(steamjson.Personaname+"'s Stats")
-	DrawText(dc, steamjson.Personaname+"'s Stats", (imageWidth-userNameWidth)/2, margin*2+10, 35)
+	DrawText(dc, steamjson.Personaname+"'s Stats", (imageWidth-userNameWidth)/2, margin*2+10, 35, color.White)
 
   if len(mods) == 0 {
-    DrawText(dc, "No mods found", 30, margin*4+10, 35)
+    DrawText(dc, "No mods found", 30, margin*4+10, 35, color.White)
   } else {
-    DrawText(dc, "Rank", 30, margin*4+10, 35)
-    DrawText(dc, "Display Name", 120, margin*4+10, 35)
-    DrawText(dc, "Downloads", imageWidth-190, margin*4+10, 35)
+    DrawText(dc, "Rank", 30, margin*4+10, 35, color.White)
+    DrawText(dc, "Display Name", 120, margin*4+10, 35, color.White)
+    DrawText(dc, "Downloads", imageWidth-190, margin*4+10, 35, color.White)
 
     dc.SetLineWidth(2)
     dc.DrawLine(30, margin*4+15, imageWidth-30, margin*4+15)
@@ -113,26 +114,55 @@ func run(steamId string, img io.Writer) error {
       _, nameTextHeight := dc.MeasureString(mods[i].DisplayName)
       dowloadsTextWidth, _ := dc.MeasureString(strconv.Itoa(mods[i].DownloadsTotal))
       
-      DrawText(dc, strconv.Itoa(mods[i].RankTotal), 30, (nameTextHeight+padding)*float64(i)+(nameTextHeight*2)+margin*4+10, 35)
-      DrawText(dc, mods[i].DisplayName, 120, (nameTextHeight+padding)*float64(i)+(nameTextHeight*2)+margin*4+10, 35)
-      DrawText(dc, strconv.Itoa(mods[i].DownloadsTotal), imageWidth-dowloadsTextWidth-50, (nameTextHeight+padding)*float64(i)+(nameTextHeight*2)+margin*4+10, 35)
+      DrawText(dc, strconv.Itoa(mods[i].RankTotal), 30, (nameTextHeight+padding)*float64(i)+(nameTextHeight*2)+margin*4+10, 35, color.White)
+
+      // NEW: parsing chat tags using regexp
+      displayNameColor, displayName := ParseChatTags(mods[i].DisplayName)
+      DrawText(dc, displayName, 120, (nameTextHeight+padding)*float64(i)+(nameTextHeight*2)+margin*4+10, 35, displayNameColor)
+
+      DrawText(dc, strconv.Itoa(mods[i].DownloadsTotal), imageWidth-dowloadsTextWidth-50, (nameTextHeight+padding)*float64(i)+(nameTextHeight*2)+margin*4+10, 35, color.White)
     }
 }
-  DrawText(dc, time.Now().Format("2006-01-02 15:04:05"), imageWidth-160, imageHeight-20, 15)
+  DrawText(dc, time.Now().Format("2006-01-02 15:04:05"), imageWidth-160, imageHeight-20, 15, color.White)
 
 	return dc.EncodePNG(img)
 }
 
-func DrawText(dc *gg.Context, s string, x float64, y float64, pnt float64) {
+func DrawText(dc *gg.Context, s string, x float64, y float64, pnt float64, col color.Color) {
 	// Load font
 	fontPath := filepath.Join("fonts", "Andy Bold.ttf")
 	dc.LoadFontFace(fontPath, pnt);
 
-	dc.SetColor(color.White)
+	dc.SetColor(col)
 	textWidth, textHeight := dc.MeasureString(s)
 	x = ClampFloat(x, 0, imageWidth-textWidth)
 	y = ClampFloat(y, textHeight, imageHeight-textHeight)
 	dc.DrawString(s, x, y)
+}
+
+func ParseChatTags(str string) (textColor color.Color, text string ){
+  var compRegEx = regexp.MustCompile(`\[c\/(?P<col>\w+):(?P<text>[\s\S]+?)\]`)
+
+  if compRegEx.MatchString(str) {
+    match := compRegEx.FindStringSubmatch(str)
+
+    paramsMap := make(map[string]string)
+    for i, name := range compRegEx.SubexpNames() {
+      if i > 0 && i <= len(match) {
+        paramsMap[name] = match[i]
+      }
+    }
+
+    b, err := hex.DecodeString(paramsMap["col"])
+    if(err != nil) {
+      
+    }
+    col := color.RGBA{b[0], b[1], b[2], 255}
+    
+    return col, paramsMap["text"]
+  }
+  
+  return color.White, str
 }
 
 func ClampFloat(v float64, min float64, max float64) float64 {
